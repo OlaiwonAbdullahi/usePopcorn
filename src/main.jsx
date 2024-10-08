78f9396cbcdfcf81d17effbbc0a413bea5409179
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-08 12:12:09 +0100

    update App.jsx and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//import App from './App.jsx'
-//import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
-import StarRatting from "./StarRatting";
+//import StarRatting from "./StarRatting";
 
-function Test() {
+/*function Test() {
   const [movieRating, setMovieRating] = useState(0);
   return (
     <div className="">
@@ -13,16 +13,16 @@ function Test() {
       <p>This Movie was rated {movieRating} stars</p>
     </div>
   );
-}
+}*/
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/*<App />*/}
-    <StarRatting
+    <App />
+    {/*<StarRatting
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amaizing"]}
     />
     <StarRatting size={24} color="green" defaultRating={3} />
-    <Test />
+    <Test />*/}
   </StrictMode>
 );
