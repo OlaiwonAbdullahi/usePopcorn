5d74efca3f3dcd7bd2ef4d43fd6acf5e1d0903be
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-08 21:54:58 +0100

    update App-v1.jsx

--- src/App-v1.jsx
+++ src/App-v1.jsx
@@ -52,11 +52,18 @@ const KEY = "10fb6f6d";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "interstellar";
 
   useEffect(function () {
-    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      console.log(data.Search);
+    }
+    fetchMovies();
   }, []);
   return (
     <>
